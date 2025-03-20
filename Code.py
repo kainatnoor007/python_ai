a7954764436cf221bdb81a83d300104da026a4b5
Kainat Noor <kainatnoor593@gmail.com>
2025-03-20 09:56:31 +0500

    python code

---
+++ Code.py
@@ -0,0 +1,107 @@
+# Quiz 1
+# Age=int(input("Enter your age:"))
+# Qualification=int(input("Qualification:"))
+# Height =float(input("Your Height:"))
+
+# if( (Age >= 18 and Age <=32) and Height >=5.6):
+#     print("Passed for interview")
+# elif((Age>=18 and Age<=32) and Qualification>=12):
+#     print("Passed for interview")
+# elif(Qualification>=12 and Height>=5.6):
+#     print("Passed for interview")
+# else:
+#     print("fail")
+
+
+# Quiz 2
+# Marks=int(input("Enter your marks: "))
+# Tmarks=int(input("Total marks: "))
+# Percentage=Marks*100/Tmarks
+# print(Percentage)
+
+# Assignment
+
+# Inc=15000
+# Percentage = 22/100
+# salary= Inc / Percentage
+# print(salary)
+
+# Inc=int(input("Enter bonus"))
+# Bonusper=int(input("Enter Percentage"))
+# Percentage= Bonusper/100
+# salary=Inc/Percentage
+# print(salary)
+
+# # Quiz 
+# Cities="jaranwala faisalabad lahore"
+# print(Cities[0:1].upper() + Cities[1:9], Cities[10:11].upper()+Cities[11:20], Cities[21:22].upper()+Cities[22:])
+
+
+# Cities="jaranwala faisalabad lahore"
+# print(Cities[0:1].lower() + Cities[1:9].upper(), Cities[10:11].lower()+Cities[11:20].upper(), Cities[21:22].lower()+Cities[22:].upper())
+
+# a="hellow"
+# b=a.replace("h","y")
+# print(b)
+
+# Quiz
+
+# City1="jaranwala"
+# Capital1=City1.replace("j","J")
+# City2="faisalabad"
+# Capital2=City2.replace("f","F")
+# City3="lahore"
+# Capital3=City3.replace("l","L")
+# print(Capital1 + " " + Capital2+" " + Capital3)
+
+
+# Assignment
+
+# City1="Jaranwala"
+# small1=City1.replace("J","j")
+# City2="Faisalabad"
+# small2=City2.replace("F","f")
+# City3="Lahore"
+# smalll3=City3.replace("L","l")
+# City4="Karachi"
+# small4=City4.replace("K", "k")
+# City5="Multan"
+# small5=City5.replace("M","m")
+# print(small1+ " "+ small2+" "+ smalll3+ " "+ small4+ " "+ small5)
+
+# Cities=["lahore", "faisalabad", "jaranwala"]
+# Cities[0]=("Lahore")
+# Cities[1]=("Faisalabad")
+# Cities[2]=("Jaranwala")
+# print(Cities)
+
+# Quiz
+# ls=["lahore", "faisalabad", "jaranwala"]
+# lp=[]
+# for city in ls:
+#     p=city[0:1].upper() + city[1:-1].lower() + city[-1:].upper()
+#     lp.append(p)
+#     print(p)
+# print(lp)
+
+# Quiz
+
+# ls=["lahore", "faisalabad", "jaranwala"]
+# lp =[]
+# for city in ls:
+#     p=city[0:1].lower() + city[1:2].upper() + city[2:-1].lower()+ city[-2:-1].upper() + city[-1:].lower()
+#     lp.append(p)
+#     print(p)
+# print(lp)
+
+# ls=["lahore", "faisalabad", "jaranwala", "peshawar", "Karachi"]
+# lp=[]
+# for cities in ls:
+#     lp.insert(0,cities)
+# print(lp)
+
+# 10 cities first reverse them and then replace first and last later are small
+
+
+
+
